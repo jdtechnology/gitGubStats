cd07605232829a5f6a7eda4dda42ea2a953cf0dc
Jack Fisher <jackfisher2012@gmail.com>
2018-03-08 17:07:03 +0000

    Separated async functions, switched to global username

--- includes/js/statsParser.js
+++ includes/js/statsParser.js
@@ -5,71 +5,76 @@
 
 statsParser.global = {
   total_additions: 0,
-  total_removals: 0
+  total_removals: 0,
+  username: ""
 };
 //todo: collect code, and pass function as callback param
 statsParser.async = {
   begin: function(username) {
+    statsParser.global.username = username;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				var userData = JSON.parse(this.responseText);
         //process();
         console.log(userData);
-        statsParser.async.parseRepo(userData, username);
+        statsParser.callback.parseRepo(userData);
 			}
 		};
-		xhttp.open("GET", "https://api.github.com/users/" + username + "/repos", true);
+		xhttp.open("GET", "https://api.github.com/users/" + statsParser.global.username + "/repos", true);
 		xhttp.send();
   },
-  parseRepo: function(repo, username) {
-    for(var i in repo) {
-      console.log(repo[i].owner.login);
-      statsParser.async.getRepo(repo[i].name, username);
-    }
-  },
-  getRepo: function(name, username) {
+  getRepo: function(name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
   			if (this.readyState == 4 && this.status == 200) {
   				var repoData = JSON.parse(this.responseText);
           console.log(repoData);
-          statsParser.async.countLines(repoData, username);
+          statsParser.callback.countLines(repoData);
   			}
   		};
-  		xhttp.open("GET", "https://api.github.com/repos/" + username + "/" + name + "/stats/contributors", true);
+  		xhttp.open("GET", "https://api.github.com/repos/" + statsParser.global.username + "/" + name + "/stats/contributors", true);
   		xhttp.send();
   },
-  getSubscriptions: function(username) {
+  getSubscriptions: function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var subData = JSON.parse(this.responseText);
-        statsParser.async.parseSubscription(subData, username);
+        statsParser.callback.parseSubscription(subData);
       }
     };
-    xhttp.open("GET", "https://api.github.com/users/" + username + "/subscriptions");
+    xhttp.open("GET", "https://api.github.com/users/" + statsParser.global.username + "/subscriptions");
     xhttp.send();
   },
-  parseSubscription: function(subData, username) {
-    for(var i in subData) {
-      statsParser.async.getSubRepo(subData[i].full_name, username);
-    }
-  },
-  getSubRepo: function(repoFName, username) {
+  getSubRepo: function(repoFName) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         var repData = JSON.parse(this.responseText);
-        statsParser.countLines(repData, username);
+        statsParser.callback.countLines(repData);
       }
     };
     xhttp.open("GET", "https://api.github.com/repos/" + repoFName + "/stats/contributors");
     xhttp.send();
   },
-  countLines: function(rData, username) {
+
+};
+statsParser.callback = {
+  parseRepo: function(repo) {
+    for(var i in repo) {
+      console.log(repo[i].owner.login);
+      statsParser.async.getRepo(repo[i].name, statsParser.global.username);
+    }
+  },
+  parseSubscription: function(subData) {
+    for(var i in subData) {
+      statsParser.async.getSubRepo(subData[i].full_name, statsParser.global.username);
+    }
+  },
+  countLines: function(rData) {
     for(var a in rData) {
-      if(rData[a].author.login == username) {
+      if(rData[a].author.login == statsParser.global.username) {
         for(var i in rData[a].weeks) {
           statsParser.global.total_additions += rData[a].weeks[i].a;
           statsParser.global.total_removals += rData[a].weeks[i].d;
